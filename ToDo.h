70c79dad95fa802fca660973573600b79b8af9e0
Shubham <theshubham1412@gmail.com>
2025-08-26 00:31:41 +0530

    started implementation of ToDo header file

---
+++ ToDo.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+class task
+{
+    int id;
+    std::string title;
+    std::string description;
+    bool status{false};
+    int prioritty;
+};
+
+class Todo_list
+{
+};
